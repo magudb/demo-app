4f802d1a65e9c94b7430fdc5173cd006a703274e
Magnus Udbjorg <magnus@udbjorg.net>
2022-09-01 15:15:02 +0200

    test: test-0017 (#22)
    * test: test-0017
    
    * test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,11 +20,19 @@ export default function Index() {
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-yellow-500 drop-shadow-md">
-                  TEST-0016
+                  Version: TEST-0017
+                </span>
+              </h1>
+            </div>
+            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
+              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+                <span className="block uppercase text-yellow-500 drop-shadow-md">
+                  Indie Stack
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-               COME ON TEST-0016
+                Check the README.md file for instructions on how to get this
+                project deployed.
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
@@ -40,13 +48,13 @@ export default function Index() {
                       to="/join"
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
                     >
-                      Join us?
+                      Sign up
                     </Link>
                     <Link
                       to="/login"
                       className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
                     >
-                      Let me in!
+                      Log In
                     </Link>
                   </div>
                 )}
