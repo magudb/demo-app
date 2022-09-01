6054207034191079084eafbdd470d7f1280e3b2e
Magnus Udbjørg <magnus@udbjorg.net>
2022-09-01 17:48:47 +0200

    fix

--- deployment/template/source.js
+++ deployment/template/source.js
@@ -2,25 +2,27 @@ import { write } from '@jkcfg/std';
 import * as param from '@jkcfg/std/param';
 
 const params = {
-    product: param.String('product', 'appname'),
-    branch: param.String('branch', 'main'),
+  product: param.String('product', 'appname'),
+  branch: param.String('branch', 'main'),
 };
 
-const kustomization = (params) =>({
-    "apiVersion": "source.toolkit.fluxcd.io/v1beta2",
-    "kind": "GitRepository",
-    "metadata": {
-        "name": `${params.product}-${params.branch}`,
-        "namespace": "flux-system"
+const kustomization = (params) => ({
+  "apiVersion": "source.toolkit.fluxcd.io/v1beta2",
+  "kind": "GitRepository",
+  "metadata": {
+    "name": `${params.product}-${params.branch}`,
+    "namespace": "flux-system"
+  },
+  "spec": {
+    "interval": "1m0s",
+    "ref": {
+      "branch": "main"
     },
-    "spec": {
-        "interval": "1m0s",
-        "path": `./clusters/kube-local/features/${params.product}-${params.branch}`,
-        "ref": {
-          "branch": "main"
-        },
-        "url": "ssh://git@github.com/magudb/fleet-infra"
-    }
-})
+    "secretRef": {
+      "name": "flux-system"
+    },
+    "url": "ssh://git@github.com/magudb/fleet-infra"
+  }
+});
 
 write(kustomization(params), `fleet-infra/clusters/kube-local/features/${params.product}-${params.branch}-source.yaml`);
